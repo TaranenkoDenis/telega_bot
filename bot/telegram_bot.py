7edced97fa84d97841dbebfe424d4c7f8aa7413e
TaranenkoDenis <taranenko.denis.serg@gmail.com>
2018-07-24 17:54:01 +0300

    fix bug

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -2,17 +2,17 @@ from bot.requests_helper import TelegaRequestsHelper
 
 
 class TelegramBot:
-    # REQUEST_SEARCH_URL = 'https://google.com/search?q='
+    REQUEST_SEARCH_URL = 'https://google.com/search?q='
 
     def __init__(self, token):
         self.requests_helper = TelegaRequestsHelper(
             "https://api.telegram.org/bot{}/".format(token)
         )
 
-    # def _get_redirect_message(self, request_text):
-    #     text = request_text.replace(' ', '+')
-    #     text = self.REQUEST_SEARCH_URL + text
-    #     return text
+    def _get_redirect_message(self, request_text):
+        text = request_text.replace(' ', '+')
+        text = self.REQUEST_SEARCH_URL + text
+        return text
 
     def start_working(self):
         answered_update_id = 0
@@ -37,7 +37,5 @@ class TelegramBot:
 
                 self.requests_helper.send_message(
                     last_chat_id, 
-                    "2" 
+                    self._get_redirect_message(last_chat_text) 
                 )
-
-                #self._get_redirect_message(last_chat_text)
